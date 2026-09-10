ca0415f3a8f97d872b706e21bce6857bc638dab4
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-09 21:47:38 -0300

    clase 09-09

--- tareasapp/urls.py
+++ tareasapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name= 'inicio'),
+    path('crear/', views.crear_tarea, name='crear_tarea'),
+    path('detalle/<int:id>/', views.detalle_tarea, name= 'detalle_tarea'),
+    path('editar/<int:id>/', views.editar_tarea, name='editar_tarea'),
+    path('eliminar', views.eliminar_tarea, name='elimar_tarea'),
 ]
